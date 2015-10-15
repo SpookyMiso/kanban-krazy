76d873e4252ee980f358d23bf6bbedcbc1b090c8
Jenn L <jenn.lazariuk@gmail.com>
2015-10-14 18:05:02 -1000

    styling finished

--- client/router.js
+++ client/router.js
@@ -1,38 +1,38 @@
 Router.configure({
-  layoutTemplate: "layout"
+  layoutTemplate: "layout"
 });
 
 Router.route('/', {
-  name: 'login',
-  onBeforeAction: function(){
-    if(Meteor.user()) {
-      this.redirect('/dashboard');
-    } else {
-      // take to dashboard
-      this.next();
-    }
-  }
+  name: 'login',
+  onBeforeAction: function(){
+    if(Meteor.user()) {
+      this.redirect('/dashboard');
+    } else {
+      // take to dashboard
+      this.next();
+    }
+  }
 });
 
 Router.route('/dashboard', function () {
-  // render the PageOne template
-  this.render('dashboard');
+  // render the PageOne template
+  this.render('dashboard');
 });
 
 Router.route('/profile/edit', function () {
-  this.render('profile');
+  this.render('profile');
 });
 
 Router.onBeforeAction(function () {
-  //if there is no user logged in and if no one is in the process of logging in
-  if(!Meteor.user() && !Meteor.loggingIn()) {
-    //take to login
-    this.render('login');
-  } else {
-    // take to dashboard
-    this.next();
-  }
+  //if there is no user logged in and if no one is in the process of logging in
+  if(!Meteor.user() && !Meteor.loggingIn()) {
+    //take to login
+    this.render('login');
+  } else {
+    // take to dashboard
+    this.next();
+  }
 }, {
-  //using template name :D
-  except: ['login']
+  //using template name :D
+  except: ['login']
 });
